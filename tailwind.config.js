d2f840407b32aafeb8525430baafa02b94ed32fa
lklein2505 <lklein00@fesb.hr>
2022-04-13 16:05:34 +0200

    Implementing tailwind and seperating navbar component

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+      extend: {
+          colors: {
+              'hci-lila': '#8034AD',
+          },
+      },
+  },
+  variants: {
+      extend: {},
+  },
+  plugins: [],
+};
