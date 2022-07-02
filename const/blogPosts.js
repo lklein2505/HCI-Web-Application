0bddeaf4ce3d52c9755b850c0fbeca6989371940
lklein2505 <lklein00@fesb.hr>
2022-07-03 00:39:07 +0200

    Adding more blog posts

--- const/blogPosts.js
+++ const/blogPosts.js
@@ -1,20 +1,122 @@
 export const blogPosts = [
     {
         title: 'My club',
-        slug: 'first',
+        slug: '1',
         date: new Date().toISOString(),
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing bibendum est ultricies integer quis auctor elit sed vulputate. Dignissim convallis aenean et tortor at risus viverra adipiscing at. Aliquet porttitor lacus luctus accumsan tortor posuere. Et malesuada fames ac turpis egestas sed. Id diam maecenas ultricies mi eget mauris pharetra. Lorem ipsum dolor sit amet. Fermentum dui faucibus in ornare quam viverra orci. Habitant morbi tristique senectus et netus et malesuada. Pulvinar etiam non quam lacus suspendisse faucibus. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien. Scelerisque eleifend donec pretium vulputate sapien nec. Nisl vel pretium lectus quam.'
     },
     {
         title: 'New victories',
-        slug: 'second',
+        slug: '2',
         date: new Date().toISOString(),
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing bibendum est ultricies integer quis auctor elit sed vulputate. Dignissim convallis aenean et tortor at risus viverra adipiscing at. Aliquet porttitor lacus luctus accumsan tortor posuere. Et malesuada fames ac turpis egestas sed. Id diam maecenas ultricies mi eget mauris pharetra. Lorem ipsum dolor sit amet. Fermentum dui faucibus in ornare quam viverra orci. Habitant morbi tristique senectus et netus et malesuada. Pulvinar etiam non quam lacus suspendisse faucibus. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien. Scelerisque eleifend donec pretium vulputate sapien nec. Nisl vel pretium lectus quam.'
     },
     {
         title: 'Bad start',
-        slug: 'third',
+        slug: '3',
         date: new Date().toISOString(),
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing bibendum est ultricies integer quis auctor elit sed vulputate. Dignissim convallis aenean et tortor at risus viverra adipiscing at. Aliquet porttitor lacus luctus accumsan tortor posuere. Et malesuada fames ac turpis egestas sed. Id diam maecenas ultricies mi eget mauris pharetra. Lorem ipsum dolor sit amet. Fermentum dui faucibus in ornare quam viverra orci. Habitant morbi tristique senectus et netus et malesuada. Pulvinar etiam non quam lacus suspendisse faucibus. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien. Scelerisque eleifend donec pretium vulputate sapien nec. Nisl vel pretium lectus quam.'
+    },
+    {
+        title: 'Overpayed players',
+        slug: '4',
+        date: new Date().toISOString(),
+        content: 'Est sit amet facilisis magna etiam tempor orci eu lobortis. In dictum non consectetur a erat nam at. Cursus vitae congue mauris rhoncus. Suscipit adipiscing bibendum est ultricies. Viverra mauris in aliquam sem fringilla. Ante metus dictum at tempor. Pharetra convallis posuere morbi leo urna. Aliquam eleifend mi in nulla. Aliquam vestibulum morbi blandit cursus risus. Id porta nibh venenatis cras. Platea dictumst vestibulum rhoncus est pellentesque. Sed faucibus turpis in eu. Viverra orci sagittis eu volutpat odio facilisis. Eros in cursus turpis massa. Sapien et ligula ullamcorper malesuada proin libero. Tortor consequat id porta nibh. Eget aliquet nibh praesent tristique magna sit. Accumsan lacus vel facilisis volutpat est. Pharetra convallis posuere morbi leo urna molestie.'
+    },
+    {
+        title: 'My main concern',
+        slug: '5',
+        date: new Date().toISOString(),
+        content: 'Est sit amet facilisis magna etiam tempor orci eu lobortis. In dictum non consectetur a erat nam at. Cursus vitae congue mauris rhoncus. Suscipit adipiscing bibendum est ultricies. Viverra mauris in aliquam sem fringilla. Ante metus dictum at tempor. Pharetra convallis posuere morbi leo urna. Aliquam eleifend mi in nulla. Aliquam vestibulum morbi blandit cursus risus. Id porta nibh venenatis cras. Platea dictumst vestibulum rhoncus est pellentesque. Sed faucibus turpis in eu. Viverra orci sagittis eu volutpat odio facilisis. Eros in cursus turpis massa. Sapien et ligula ullamcorper malesuada proin libero. Tortor consequat id porta nibh. Eget aliquet nibh praesent tristique magna sit. Accumsan lacus vel facilisis volutpat est. Pharetra convallis posuere morbi leo urna molestie.'    
+    },
+    {
+        title: 'Powerful opponents',
+        slug: '6',
+        date: new Date().toISOString(),
+        content: 'Est sit amet facilisis magna etiam tempor orci eu lobortis. In dictum non consectetur a erat nam at. Cursus vitae congue mauris rhoncus. Suscipit adipiscing bibendum est ultricies. Viverra mauris in aliquam sem fringilla. Ante metus dictum at tempor. Pharetra convallis posuere morbi leo urna. Aliquam eleifend mi in nulla. Aliquam vestibulum morbi blandit cursus risus. Id porta nibh venenatis cras. Platea dictumst vestibulum rhoncus est pellentesque. Sed faucibus turpis in eu. Viverra orci sagittis eu volutpat odio facilisis. Eros in cursus turpis massa. Sapien et ligula ullamcorper malesuada proin libero. Tortor consequat id porta nibh. Eget aliquet nibh praesent tristique magna sit. Accumsan lacus vel facilisis volutpat est. Pharetra convallis posuere morbi leo urna molestie.'    
+    },
+    {
+        title: 'Finally good game',
+        slug: '7',
+        date: new Date().toISOString(),
+        content: 'Tortor aliquam nulla facilisi cras fermentum odio eu feugiat pretium. Ac ut consequat semper viverra nam libero justo laoreet sit. Ut lectus arcu bibendum at varius vel pharetra vel turpis. Hac habitasse platea dictumst vestibulum rhoncus est. Eu mi bibendum neque egestas congue quisque egestas diam. Aliquet lectus proin nibh nisl condimentum. Arcu bibendum at varius vel pharetra.'
+    },
+    {
+        title: 'President on the press',
+        slug: '8',
+        date: new Date().toISOString(),
+        content: 'Tortor aliquam nulla facilisi cras fermentum odio eu feugiat pretium. Ac ut consequat semper viverra nam libero justo laoreet sit. Ut lectus arcu bibendum at varius vel pharetra vel turpis. Hac habitasse platea dictumst vestibulum rhoncus est. Eu mi bibendum neque egestas congue quisque egestas diam. Aliquet lectus proin nibh nisl condimentum. Arcu bibendum at varius vel pharetra.'
+    },
+    {
+        title: 'Mighty Vagons',
+        slug: '9',
+        date: new Date().toISOString(),
+        content: 'Tortor aliquam nulla facilisi cras fermentum odio eu feugiat pretium. Ac ut consequat semper viverra nam libero justo laoreet sit. Ut lectus arcu bibendum at varius vel pharetra vel turpis. Hac habitasse platea dictumst vestibulum rhoncus est. Eu mi bibendum neque egestas congue quisque egestas diam. Aliquet lectus proin nibh nisl condimentum. Arcu bibendum at varius vel pharetra.'
+    },
+    {
+        title: 'Chu Chu',
+        slug: '10',
+        date: new Date().toISOString(),
+        content: 'Tortor aliquam nulla facilisi cras fermentum odio eu feugiat pretium. Ac ut consequat semper viverra nam libero justo laoreet sit. Ut lectus arcu bibendum at varius vel pharetra vel turpis. Hac habitasse platea dictumst vestibulum rhoncus est. Eu mi bibendum neque egestas congue quisque egestas diam. Aliquet lectus proin nibh nisl condimentum. Arcu bibendum at varius vel pharetra.'
+    },
+    {
+        title: 'Vamooos!',
+        slug: '11',
+        date: new Date().toISOString(),
+        content: 'Quis risus sed vulputate odio ut enim blandit volutpat. Nisi porta lorem mollis aliquam ut. In pellentesque massa placerat duis ultricies lacus. A scelerisque purus semper eget duis at. Posuere urna nec tincidunt praesent. Pulvinar mattis nunc sed blandit libero volutpat sed cras. Eu feugiat pretium nibh ipsum consequat. Pharetra massa massa ultricies mi. Sodales ut eu sem integer vitae justo. Est placerat in egestas erat. Eu ultrices vitae auctor eu augue ut lectus arcu.'    
+    },
+    {
+        title: 'High highes, low lowes',
+        slug: '12',
+        date: new Date().toISOString(),
+        content: 'Est sit amet facilisis magna etiam tempor orci eu lobortis. In dictum non consectetur a erat nam at. Cursus vitae congue mauris rhoncus. Suscipit adipiscing bibendum est ultricies. Viverra mauris in aliquam sem fringilla. Ante metus dictum at tempor. Pharetra convallis posuere morbi leo urna. Aliquam eleifend mi in nulla. Aliquam vestibulum morbi blandit cursus risus. Id porta nibh venenatis cras. Platea dictumst vestibulum rhoncus est pellentesque. Sed faucibus turpis in eu. Viverra orci sagittis eu volutpat odio facilisis. Eros in cursus turpis massa. Sapien et ligula ullamcorper malesuada proin libero. Tortor consequat id porta nibh. Eget aliquet nibh praesent tristique magna sit. Accumsan lacus vel facilisis volutpat est. Pharetra convallis posuere morbi leo urna molestie.'    
+    },
+    {
+        title: 'What a game...',
+        slug: '13',
+        date: new Date().toISOString(),
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing bibendum est ultricies integer quis auctor elit sed vulputate. Dignissim convallis aenean et tortor at risus viverra adipiscing at. Aliquet porttitor lacus luctus accumsan tortor posuere. Et malesuada fames ac turpis egestas sed. Id diam maecenas ultricies mi eget mauris pharetra. Lorem ipsum dolor sit amet. Fermentum dui faucibus in ornare quam viverra orci. Habitant morbi tristique senectus et netus et malesuada. Pulvinar etiam non quam lacus suspendisse faucibus. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien. Scelerisque eleifend donec pretium vulputate sapien nec. Nisl vel pretium lectus quam.'
+    },
+    {
+        title: 'Good performance',
+        slug: '14',
+        date: new Date().toISOString(),
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing bibendum est ultricies integer quis auctor elit sed vulputate. Dignissim convallis aenean et tortor at risus viverra adipiscing at. Aliquet porttitor lacus luctus accumsan tortor posuere. Et malesuada fames ac turpis egestas sed. Id diam maecenas ultricies mi eget mauris pharetra. Lorem ipsum dolor sit amet. Fermentum dui faucibus in ornare quam viverra orci. Habitant morbi tristique senectus et netus et malesuada. Pulvinar etiam non quam lacus suspendisse faucibus. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien. Scelerisque eleifend donec pretium vulputate sapien nec. Nisl vel pretium lectus quam.'
+    },
+    {
+        title: 'Where is VAR?',
+        slug: '15',
+        date: new Date().toISOString(),
+        content: 'Quis risus sed vulputate odio ut enim blandit volutpat. Nisi porta lorem mollis aliquam ut. In pellentesque massa placerat duis ultricies lacus. A scelerisque purus semper eget duis at. Posuere urna nec tincidunt praesent. Pulvinar mattis nunc sed blandit libero volutpat sed cras. Eu feugiat pretium nibh ipsum consequat. Pharetra massa massa ultricies mi. Sodales ut eu sem integer vitae justo. Est placerat in egestas erat. Eu ultrices vitae auctor eu augue ut lectus arcu.'    
+    },
+    {
+        title: 'All done',
+        slug: '16',
+        date: new Date().toISOString(),
+        content: 'Quis risus sed vulputate odio ut enim blandit volutpat. Nisi porta lorem mollis aliquam ut. In pellentesque massa placerat duis ultricies lacus. A scelerisque purus semper eget duis at. Posuere urna nec tincidunt praesent. Pulvinar mattis nunc sed blandit libero volutpat sed cras. Eu feugiat pretium nibh ipsum consequat. Pharetra massa massa ultricies mi. Sodales ut eu sem integer vitae justo. Est placerat in egestas erat. Eu ultrices vitae auctor eu augue ut lectus arcu.'    
+    },
+    {
+        title: 'Referee decided the game',
+        slug: '17',
+        date: new Date().toISOString(),
+        content: 'Sed turpis tincidunt id aliquet risus. Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus. Nibh sit amet commodo nulla. A arcu cursus vitae congue mauris. Nisi porta lorem mollis aliquam. In hac habitasse platea dictumst vestibulum rhoncus est. Vivamus at augue eget arcu dictum varius. Venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin. Tincidunt tortor aliquam nulla facilisi cras. Aliquet nibh praesent tristique magna sit amet purus gravida. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras. Proin libero nunc consequat interdum varius sit amet mattis vulputate. Blandit volutpat maecenas volutpat blandit aliquam. Aenean euismod elementum nisi quis eleifend. Lacus vestibulum sed arcu non odio euismod lacinia at quis.'    
+    },
+    {
+        title: 'Sei bella come un GOL al 90',
+        slug: '18',
+        date: new Date().toISOString(),
+        content: 'Sed turpis tincidunt id aliquet risus. Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus. Nibh sit amet commodo nulla. A arcu cursus vitae congue mauris. Nisi porta lorem mollis aliquam. In hac habitasse platea dictumst vestibulum rhoncus est. Vivamus at augue eget arcu dictum varius. Venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin. Tincidunt tortor aliquam nulla facilisi cras. Aliquet nibh praesent tristique magna sit amet purus gravida. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras. Proin libero nunc consequat interdum varius sit amet mattis vulputate. Blandit volutpat maecenas volutpat blandit aliquam. Aenean euismod elementum nisi quis eleifend. Lacus vestibulum sed arcu non odio euismod lacinia at quis.'    
+    },
+    {
+        title: 'Selling and buying',
+        slug: '19',
+        date: new Date().toISOString(),
+        content: 'Sed turpis tincidunt id aliquet risus. Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus. Nibh sit amet commodo nulla. A arcu cursus vitae congue mauris. Nisi porta lorem mollis aliquam. In hac habitasse platea dictumst vestibulum rhoncus est. Vivamus at augue eget arcu dictum varius. Venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin. Tincidunt tortor aliquam nulla facilisi cras. Aliquet nibh praesent tristique magna sit amet purus gravida. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras. Proin libero nunc consequat interdum varius sit amet mattis vulputate. Blandit volutpat maecenas volutpat blandit aliquam. Aenean euismod elementum nisi quis eleifend. Lacus vestibulum sed arcu non odio euismod lacinia at quis.'    
+    },
+    {
+        title: 'New year, new stars',
+        slug: '20',
+        date: new Date().toISOString(),
+        content: 'Est sit amet facilisis magna etiam tempor orci eu lobortis. In dictum non consectetur a erat nam at. Cursus vitae congue mauris rhoncus. Suscipit adipiscing bibendum est ultricies. Viverra mauris in aliquam sem fringilla. Ante metus dictum at tempor. Pharetra convallis posuere morbi leo urna. Aliquam eleifend mi in nulla. Aliquam vestibulum morbi blandit cursus risus. Id porta nibh venenatis cras. Platea dictumst vestibulum rhoncus est pellentesque. Sed faucibus turpis in eu. Viverra orci sagittis eu volutpat odio facilisis. Eros in cursus turpis massa. Sapien et ligula ullamcorper malesuada proin libero. Tortor consequat id porta nibh. Eget aliquet nibh praesent tristique magna sit. Accumsan lacus vel facilisis volutpat est. Pharetra convallis posuere morbi leo urna molestie.'    
     },
 ]
