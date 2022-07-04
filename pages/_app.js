023ec9ad78c0de6f36def15d5b7358ab6bcbc057
lklein2505 <lklein00@fesb.hr>
2022-07-04 16:51:26 +0200

    Adding page title

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Head from 'next/dist/shared/lib/head';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import { AuthContextProvider } from '../stores/authContext';
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthContextProvider>
+        <Head>
+          <title>NK Vagon</title>
+          <meta property="og:title" content="My page title" key="title" />
+      </Head>
       <Component {...pageProps} />
     </AuthContextProvider>
   )
